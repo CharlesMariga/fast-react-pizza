f1103ae9e1207ea9d2bf2de91c65f40dc4119c98
CharlesMariga <charlesmariga37@gmail.com>
2023-12-20 07:04:28 +0300

    feat: displaying loading indicator

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -6,7 +6,7 @@ import Loader from "./Loader";
 
 export default function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state.isLoading === "loading";
+  const isLoading = navigation.state === "loading";
 
   return (
     <div className="layout">
