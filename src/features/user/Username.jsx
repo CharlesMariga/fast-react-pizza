fc40a65150e7395457315bae91ae1c05a1a6bf51
CharlesMariga <charlesmariga37@gmail.com>
2023-12-21 09:21:51 +0300

    feat: styling text

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Username() {
+  return <p className="text-sm font-semibold">Charles</p>;
+}
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Feat React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Feat React Pizza Co.
+      </Link>
 
       <SearchOrder />
 
-      <p>Charles</p>
+      <Username />
     </header>
   );
 }
